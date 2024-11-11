0ebcde4ad52f6a17b6dafc3080e377fa3bbce496
saba farzanegan <saba.far.99@gmail.com>
2024-11-11 21:31:35 +0330

    add inital structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="text-xl font-lale ">
+      سلام
+      <Button>کلیک</Button>
+    </div>
+  );
+}
+
+export default App;
